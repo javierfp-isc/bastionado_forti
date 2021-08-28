5c982c2a0a251ddea2981db808677d7b45d30a30
Javier Fernández Peón <javierfp@iessanclemente.net>
2021-08-28 19:55:28 +0200

    arranque de filebeat

--- iproxy/compose/build/entrypoint.sh
+++ iproxy/compose/build/entrypoint.sh
@@ -14,6 +14,9 @@ cd /ser8081 && python -m SimpleHTTPServer 8081 &
 #Arrancamos nginx
 [ -f /etc/nginx/nginx.conf ] && /usr/sbin/nginx -g 'daemon on; master_process on;'
 
+#Arrancamos filebeat
+[ -f /etc/init.d/filebeat ] && /etc/init.d/filebeat start
+
 #Uso exec para lanzar un proceso independiente de bucle infinito
 exec bash -c "while true;do sleep 10;done"
 
