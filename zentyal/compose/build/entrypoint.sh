7ac31438cad89b21cded3251b1ca72e235aa973b
Javier Fernández Peón <javierfp@iessanclemente.net>
2021-09-22 21:55:06 +0200

    Escenario zentyal

--- zentyal/compose/build/entrypoint.sh
+++ zentyal/compose/build/entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-exec /lib/systemd/systemd
+#Si existe el archivo /etc/routes lo ejecuta
+[ -f /etc/routes ] && sh /etc/routes
+
+#Establece los servidores DNS a 8.8.8.8 y 8.8.4.4
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+
+#Uso exec para lanzar un proceso independiente de bucle infinito
+exec bash -c "while true;do sleep 10;done"
+
+#exec /lib/systemd/systemd
 
