37ec91b53a6a3301ee311ffb152187d3762bd46d
Javier Fernández Peón <javierfp@iessanclemente.net>
2021-10-02 01:52:15 +0200

    Backup

--- zentyal/compose/build/entrypoint.sh
+++ zentyal/compose/build/entrypoint.sh
@@ -4,11 +4,11 @@
 [ -f /etc/routes ] && sh /etc/routes
 
 #Establece los servidores DNS a 8.8.8.8 y 8.8.4.4
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
-echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+#echo "nameserver 8.8.8.8" > /etc/resolv.conf
+#echo "nameserver 8.8.4.4" >> /etc/resolv.conf
 
 #Uso exec para lanzar un proceso independiente de bucle infinito
-exec bash -c "while true;do sleep 10;done"
+#exec bash -c "while true;do sleep 10;done"
 
-#exec /lib/systemd/systemd
+exec /lib/systemd/systemd
 
