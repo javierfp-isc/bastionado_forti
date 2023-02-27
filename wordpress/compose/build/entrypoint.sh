4d05e94e742a7cc3999ab7e3414932b21ff33670
javierfp <javierfp@a21eql00.sanclemente.local>
2023-02-27 12:43:39 +0100

    bullseye

--- wordpress/compose/build/entrypoint.sh
+++ wordpress/compose/build/entrypoint.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-#Arrancamos apache y mysql
-[ -d /var/lib/mysql ] && service mysql start
+#Arrancamos apache
 [ -d /var/www/html ] && service apache2 start
 
-#Arrancamos ssh
-[ -f /etc/ssh/sshd_config ] && service ssh start
+#Arranque de mariadb y creación de BBDD
+if [ -d /var/lib/mysql ]
+then
+	service mariadb start
+	#Crea la base de datos si no existe
+	mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $WPDB"
+	#Crea el usuario WPUSER si no existe y le otorga permisos a la base de datos
+	if [ $(mysql -uroot -D mysql -s -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$WPUSER')") ]
+	then
+		mysql -uroot -e "GRANT ALL ON $WPDB.* TO '$WPUSER'@'%' IDENTIFIED BY '$WPPASS'"
+	fi
+fi
 
 #Uso exec para lanzar un proceso independiente de bucle infinito
 exec bash -c "while true;do sleep 10;done"
