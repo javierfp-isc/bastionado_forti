931b99f2bca9bf3459f2baa3f2f2d74382b41a7c
javierfp <javierfp@infol05.sanclemente.local>
2025-02-13 14:22:18 +0100

    cambio version zabbix

---
+++ zabbix_5/compose/build/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Arrancamos apache y mysql
+[ -d /var/lib/mysql ] && service mysql start
+[ -d /var/www/html ] && service apache2 start
+
+#Arrancamos zabbix
+[ -f /etc/init.d/zabbix-server ] && /etc/init.d/zabbix-server start
+[ -f /etc/init.d/zabbix-agent ] && /etc/init.d/zabbix-agent start
+
+#Uso exec para lanzar un proceso independiente de bucle infinito
+exec bash -c "while true;do sleep 10;done"
+
